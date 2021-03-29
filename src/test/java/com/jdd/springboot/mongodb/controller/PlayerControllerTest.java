c7282d517af04430c6445c9402c93b5b8ac17c47
jondoetsch27 <jonathon.doetsch@gmail.com>
2021-03-29 09:08:51 -0700

    03/29 - deleted old PlayerControllerTest and renamed PlayerControllerMockTest @JDD

--- src/test/java/com/jdd/springboot/mongodb/controller/PlayerControllerTest.java
+++ src/test/java/com/jdd/springboot/mongodb/controller/PlayerControllerTest.java
@@ -1,27 +1,29 @@
 package com.jdd.springboot.mongodb.controller;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.jdd.springboot.mongodb.model.Player;
-import com.jdd.springboot.mongodb.model.Player.Builder;
-import java.net.URL;
+import com.jdd.springboot.mongodb.service.impl.PlayerServiceImpl;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
-import org.springframework.boot.test.web.client.TestRestTemplate;
-import org.springframework.boot.web.server.LocalServerPort;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.test.web.servlet.MockMvc;
 
-import static org.assertj.core.api.Assertions.assertThat;
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
-@SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT)
+@WebMvcTest(controllers = PlayerController.class)
 public class PlayerControllerTest {
 
-  @LocalServerPort private int port;
+  @Autowired private MockMvc mockMvc;
 
-  @Autowired private TestRestTemplate restTemplate;
+  @Autowired private ObjectMapper objectMapper;
 
+  @MockBean private PlayerServiceImpl playerService;
+
+  private static final String APPLICATION_JSON = "application/json";
   private static final String DELETE_URL = "/players/delete";
   private static final String GET_URL = "/players/read";
   private static final String GET_ALL_URL = "/players/read-all";
@@ -31,36 +33,46 @@ public class PlayerControllerTest {
 
   @Test
   public void readPlayersTest() throws Exception {
-    ResponseEntity<Iterable> iterableResponseEntity =
-        restTemplate.getForEntity(
-            new URL("http://localhost:" + port + GET_ALL_URL).toString(), Iterable.class);
-    assertEquals(HttpStatus.OK, iterableResponseEntity.getStatusCode());
+    mockMvc.perform(get(GET_ALL_URL).contentType(APPLICATION_JSON)).andExpect(status().isOk());
   }
 
   @Test
   public void readPlayerTest() throws Exception {
-    ResponseEntity<Player> playerResponseEntity =
-        restTemplate.getForEntity(
-            new URL("http://localhost:" + port + GET_URL + PLAYER_ID_URL + "MatthewStafford9")
-                .toString(),
-            Player.class);
-    assertEquals(HttpStatus.FOUND, playerResponseEntity.getStatusCode());
-    assertThat(playerResponseEntity.getBody().getPlayerId().equals("MatthewStafford9"));
+    mockMvc
+        .perform(get(GET_URL + PLAYER_ID_URL + "MatthewStafford9").contentType(APPLICATION_JSON))
+        .andExpect(status().isFound());
   }
 
   @Test
   public void createPlayerTest() throws Exception {
-    Player testPlayer = new Player("TerryMcLaurin17", "TerryMcLaurin", "17");
-    ResponseEntity<Player> playerResponseEntity =
-        restTemplate.postForEntity(
-            new URL("http://localhost:" + port + POST_URL).toString(), testPlayer, Player.class);
-    assertEquals(HttpStatus.CREATED, playerResponseEntity.getStatusCode());
-    assertThat(playerResponseEntity.getBody().getPlayerId().equals("TerryMcLaurin17"));
+    Player player = new Player("RussellWilson3", "RussellWilson", "3");
+    mockMvc
+        .perform(
+            post(POST_URL)
+                .content(objectMapper.writeValueAsString(player))
+                .contentType(APPLICATION_JSON))
+        .andExpect(status().isCreated());
   }
 
   @Test
-  public void updatePlayerTest() throws Exception {}
+  public void updatePlayerTest() throws Exception {
+    Player player = new Player("RussellWilson3", "RussWilson", "3");
+    mockMvc
+        .perform(
+            put(PUT_URL)
+                .content(objectMapper.writeValueAsString(player))
+                .contentType(APPLICATION_JSON))
+        .andExpect(status().isAccepted());
+  }
 
   @Test
-  public void deletePlayerTest() throws Exception {}
+  public void deletePlayerTest() throws Exception {
+    Player player = new Player("RussellWilson3", "RussWilson", "3");
+    mockMvc
+        .perform(
+            delete(DELETE_URL)
+                .content(objectMapper.writeValueAsString(player))
+                .contentType(APPLICATION_JSON))
+        .andExpect(status().isAccepted());
+  }
 }
